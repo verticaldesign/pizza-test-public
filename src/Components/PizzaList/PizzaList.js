6f2c7c6969b43cb1dbefda34a7b200717afc1033
erictuftee <erictuftee@gmail.com>
2018-10-08 18:18:01 -0500

    test: tests if pizzas are loaded

--- src/Components/PizzaList/PizzaList.js
+++ src/Components/PizzaList/PizzaList.js
@@ -3,25 +3,44 @@ import PropTypes from "prop-types";
 
 const PizzaList = props => {
   if (props.loaded) {
-    return <div>{renderPizzaList(props.pizzas)}</div>;
+    return (
+      <div className="pizza-list-container">
+        {renderPizzaList(props.pizzas, props.filterText, props.sorting)}
+      </div>
+    );
   } else {
-    return <div>loading</div>;
+    return <div className="pizza-list-container loading">loading</div>;
   }
 };
 
-function renderPizzaList(pizzas) {
-  let pizzaList = pizzas.map(pizza => {
-    return <div>{pizza}</div>;
+function renderPizzaList(pizzas, filterText, sorting) {
+  let pizzaList = pizzas.slice();
+  if (filterText != "") {
+    pizzaList = pizzaList.filter(p =>
+      p.toUpperCase().includes(filterText.toUpperCase())
+    );
+  }
+  pizzaList = pizzaList.map(pizza => {
+    return (
+      <div className="pizza-item" key={pizza}>
+        {pizza}
+      </div>
+    );
   });
   return pizzaList;
 }
+
 PizzaList.defaultProps = {
   pizzas: [],
-  loaded: false
+  loaded: false,
+  filterText: "",
+  sorting: 0
 };
 PizzaList.propTypes = {
   pizzas: PropTypes.arrayOf(PropTypes.string),
-  loaded: PropTypes.bool
+  loaded: PropTypes.bool,
+  filterText: PropTypes.string,
+  sorting: PropTypes.number
 };
 
 export default PizzaList;
--- src/__tests__/PizzaList/PizzaList.test.js
+++ src/__tests__/PizzaList/PizzaList.test.js
@@ -3,12 +3,29 @@ import { expect } from "code";
 import { shallow } from "enzyme";
 import PizzaList from "../../components/PizzaList/PizzaList";
 
-describe("Given PizzaList", () => {
+describe("Given ,<PizzaList>", () => {
   let component;
   beforeEach(() => {
     component = shallow(<PizzaList />);
   });
   it("should exist", () => {
     expect(component.exists()).to.be.true();
+  });
+  it("should have class of pizza-list-container", () => {
+    expect(component.hasClass("pizza-list-container")).to.be.true();
+  });
+  describe("When pizzas are not loaded", () => {
+    it("should display a loading message", () => {
+      expect(component.hasClass("loading")).to.be.true();
+    });
+  });
+  describe("When pizzas are given", () => {
+    function renderComponent(props = {}) {
+      return shallow(<PizzaList loaded={true} {...props} />);
+    }
+    it("should display the list of pizzas with class of pizza-item", () => {
+      let renderedComponent = renderComponent({ pizzas: ["peperoni", "sss"] });
+      expect(renderedComponent.find(".pizza-item").length).to.be.greaterThan(0);
+    });
   });
 });
