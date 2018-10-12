fe3d64b49dedab34034e2d605a09a8ddde460d83
erictuftee <erictuftee@gmail.com>
2018-10-12 10:57:53 -0500

    Updates

---
+++ precache-manifest.3d0106e5b71b29eb3275573096788d7a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "e69c940054390cd26cc2e60aa1b9d54c",
+    "url": "/pizza-test-public/static/media/pizza_illustration.e69c9400.png"
+  },
+  {
+    "revision": "240922be425c015f9124a84f46cc3968",
+    "url": "/pizza-test-public/static/media/pizza.240922be.jpg"
+  },
+  {
+    "revision": "6f6f9a288a25c9cc905028de7fde021b",
+    "url": "/pizza-test-public/static/media/pizza-SVG.6f6f9a28.svg"
+  },
+  {
+    "revision": "9bf10aab5ce1bd5f9071",
+    "url": "/pizza-test-public/static/js/runtime~main.9bf10aab.js"
+  },
+  {
+    "revision": "80a33ed5ef192e89febc",
+    "url": "/pizza-test-public/static/js/main.80a33ed5.chunk.js"
+  },
+  {
+    "revision": "ad4591f84e9b761fa829",
+    "url": "/pizza-test-public/static/js/1.ad4591f8.chunk.js"
+  },
+  {
+    "revision": "80a33ed5ef192e89febc",
+    "url": "/pizza-test-public/static/css/main.d216efca.chunk.css"
+  },
+  {
+    "revision": "a450324b48daa452639fd281d0cb7984",
+    "url": "/pizza-test-public/index.html"
+  }
+];
